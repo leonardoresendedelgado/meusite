03b41022e594a0195d5c1f8098fe60bf297ff85b
leonardoresendelgado <profissional.l.delgado@outlook.com>
2022-08-09 16:33:16 -0300

    criacao func maq escrever/ mudancas css

--- maqescrever.js
+++ maqescrever.js
@@ -1,12 +1,45 @@
-function typeWriter(elemento){
-    const textoArray = elemento.innerHTML.split('');
-    elemento.innerHTML = '';
-    textoArray.forEach((letra, i)=>{
-        setTimeout(() =>
-    elemento.innerHTML += letra, 150 * i)
-    });
-        
-    
+if ($('#typewriter').length > 0) {
+    var textContainer = document.querySelector('#typewriter');
+    var textWhiter = ['Desenvolvedor backend.', 'Administrador Azure.',  ];
+
+    function whiter(str, done) {
+        var char = str.split('').reverse();
+        var typer = setInterval(function () {
+            if (!char.length) {
+                clearInterval(typer);
+                return setTimeout(done, 1500); 
+            }
+            var next = char.pop();
+            textContainer.innerHTML += next;
+        }, 100);
+    }
+
+    function cleaner(done) {
+        var char = textContainer.innerHTML;
+        var nr = char.length;
+        var typer = setInterval(function () {
+            if (nr-- === 0) {
+                clearInterval(typer);
+                return done();
+            }
+            textContainer.innerHTML = char.slice(0, nr);
+        }, 50);
+    }
+
+    function footer(content) {
+        var atual = -1;
+        function prox() {
+            if (atual < content.length - 1)
+                atual++;
+            else
+                atual = 0;
+            var str = content[atual];
+            whiter(str, function () {
+                cleaner(prox);
+            });
+        }
+        prox(prox);
+    }
+
+    footer(textWhiter);
 }
-const curriculo = document.querySelector('h1');
-typeWriter(curriculo);
